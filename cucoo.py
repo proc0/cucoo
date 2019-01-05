3e488ede285ff28fc31f6d5ed14de4fab2b44e5d
proc0 <alanvx@gmail.com>
2019-01-04 23:04:51 -0800

    dev: abstracting functions to unit test

--- cucoo.py
+++ cucoo.py
@@ -3,55 +3,56 @@ import math
 import time
 import datetime
 import os
-import winsound
 
+TOTAL_MIN = 60
+TOTAL_SEC = 3600
+TOTAL_HRS = 8760
 YEAR = time.localtime().tm_year
 NYE = datetime.datetime(YEAR, 1, 1, 0, 0, 0)
 
 # hours since last NYE midnight
-get_delta = lambda year: int(math.floor((time.time() - time.mktime(year.timetuple()))/3600))
+get_delta = lambda year, now: int(math.floor((now - time.mktime(year.timetuple()))/TOTAL_SEC))
 # hours until new year
-get_remains = lambda elaps: 8760 - elaps
+get_remains = lambda hours: TOTAL_HRS - hours
 # seconds until next hour (minus check time)
-get_sleeptime = lambda local: (60 - local.tm_min)*60 - (local.tm_sec + 1)
+get_sleeptime = lambda now: (TOTAL_MIN - now.tm_min)*TOTAL_MIN - (now.tm_sec + 1)
 
 # output functions
-def beep():
-  if(winsound):
-    winsound.Beep(1500, 500)
-    winsound.Beep(1500, 500)
+def cuckoo():
+  if(os.sys.platform == 'win32'):
+    import winsound
+    winsound.PlaySound('cuckoo.wav', winsound.SND_FILENAME)
   else:
-    os.system("echo -n '\a'")
-    os.system("echo -n '\a'")
+    os.system('echo -n "\a"')
 
-def cucoo(hours):
-  beep()
-  print('+------------------------------------------+')
+def alert(hours):
+  cuckoo()
+  print('+-----------------------------------------+')
   print(' ' + str(get_remains(hours)) + ' hours remain on the ' + str(hours) + '* hour of ' + str(YEAR))
 
 def countdown():
   # NEXT: implement second countdown
   print('Last hour of the year!')
   for _ in range(0,5):
-    beep()
+    cuckoo()
 # --
 
-def main():
-  delta = get_delta(NYE)
-  cucoo(delta)
+def main(NOW = lambda: time.time()):
+  delta = get_delta(NYE, NOW())
+  alert(delta)
   # check until time delta is negative
-  while(get_delta(NYE) - delta >= 0):
-    theta = get_delta(NYE)
+  while(get_delta(NYE, NOW()) - delta >= 0):
+    epsilon = get_delta(NYE, NOW())
     # hour o'clock
-    if(theta - delta == 1):
+    if(epsilon - delta == 1):
       # update delta
-      delta = theta
-      cucoo(delta)
+      delta = epsilon
+      alert(delta)
       # wait til next check
-      time.sleep(3599)
+      time.sleep(TOTAL_SEC - 1)
     else:
       # update delta
-      delta = get_delta(NYE)
+      delta = get_delta(NYE, NOW())
       sleep_time = get_sleeptime(time.localtime())
       # wait if more than a second
       if(sleep_time > 1):
